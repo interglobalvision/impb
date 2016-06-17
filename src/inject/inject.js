a523c8299b952d9b5f43c4967b9d726213d0dd3e
Michael Ray-Von <mikey@interglobal.vision>
2016-06-17 17:15:04 -0500

    rewrite script in vanilla js

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -3,21 +3,37 @@ chrome.extension.sendMessage({}, function(response) {
     if (document.readyState === "complete") {
       clearInterval(readyStateCheckInterval);
 
-      $('#main_bottom').prepend('<div id="impb"></div>'); // make the container element
+      var impbContainer = document.getElementById('main_bottom');
+      var impb = document.createElement('div');
 
       // make it IMDB style
-      $('#impb').css('padding', '20px 21px 24px 20px')
-      .html('<h3>Pirate Bay Search Results</h3><div class="impb-loading">Loading...</div>');
+      impb.id = 'impb';
+      impb.style.padding = '20px 21px 24px 20px';
 
-      var title = encodeURI($('.title_wrapper h1').text());
+      impbContainer.insertBefore( impb, impbContainer.firstChild );
 
-      var ogType = $('meta[property="og:type"').attr('content');
+      // add the heading
+      var heading = document.createElement('h3');
+      heading.innerHTML = 'Pirate Bay Search Results';
+      impb.appendChild( heading );
+
+      // add content wrapper with loading text
+      var impbContent = document.createElement('div');
+      impbContent.id = 'impb-content';
+      impbContent.innerHTML = 'Loading...';
+      impb.appendChild( impbContent );
+
+      // get the title
+      var title = encodeURI( document.body.querySelector('.title_wrapper h1').textContent );
+
+      // get the type of video
+      var ogType = document.querySelector('meta[property="og:type"').getAttribute('content');
       var type = ogType.slice(6);
 
       // form tv episode torrent naming convention
       if (type === 'episode') {
-        var show = $('.titleParent a').text();
-        var seasonEpisode = $('.bp_heading').html().split(' ');
+        var show = document.body.querySelector('.titleParent a').textContent;
+        var seasonEpisode = document.body.querySelector('.bp_heading').textContent.split(' ');
         var season = seasonEpisode[1];
         var episode = seasonEpisode.reverse()[0];
 
@@ -29,52 +45,115 @@ chrome.extension.sendMessage({}, function(response) {
           episode = '0' + episode;
         }
 
-        title = encodeURI(show + ' S' + season + 'E' + episode); // whoop!
+        title = encodeURI( show + ' S' + season + 'E' + episode ); // whoop!
       }
 
+      // what were searching for
       var searchTerm = title;
 
-      $.ajax({
-        url: "https://thepiratebay.org/search/" + searchTerm,
-        dataType: 'text',
-        success: function(data) {
-          var $searchResult = $(data).find('#searchResult'); // find the search results table
+      // open a new Http Request
+      var request = new XMLHttpRequest();
+      request.open('GET', 'https://thepiratebay.org/search/' + searchTerm, true);
 
-          if ($searchResult.length) { // stuff!
+      // do this when the request returns
+      request.onload = function() {
+        if (request.status >= 200 && request.status < 400) {
+          // Success!
+          var container = document.implementation.createHTMLDocument('');
+          container.open();
+          container.write(request.responseText);
+          container.close();
+
+          var searchResult = container.getElementById('searchResult');
+
+          if (searchResult) { // stuff!
 
             // edit the table headings
-            $searchResult.find('th:first-of-type').html('Type');
-            $searchResult.find('.sortby').html('Name');
-            $searchResult.find('.viewswitch').remove();
-            $searchResult.find('[title="Seeders"]').html('Seed');
-            $searchResult.find('[title="Leechers"]').html('Leech');
+            searchResult.querySelector('th:first-of-type').innerHTML = 'Type';
+            searchResult.querySelector('.sortby').innerHTML = 'Name';
+            searchResult.querySelector('.viewswitch').remove();
+            searchResult.querySelector('abbr[title="Seeders"]').innerHTML = 'Seed';
+            searchResult.querySelector('abbr[title="Leechers"]').innerHTML = 'Leech';
 
-            $searchResult.find('tbody tr:gt(9)').remove(); // keep only the top 10
+            // keep only the top 10
+            var rows = searchResult.querySelectorAll('tbody tr');
 
-            $searchResult.find('td.vertTh a:link').contents().unwrap(); // remove 'Type' links
 
-            $searchResult.find('.detName a:link, td a[href*="/user"]').attr('href', function(index, src) {
-              return 'https://thepiratebay.org' + src;
-            }); // add the Pirate Day domain to User links
+            for (i = 0; i < rows.length; i++) {
+              if (i > 9) {
+                rows[i].parentNode.removeChild(rows[i]);
+              } else {
+                if (i % 2) {
+                  rows[i].classList.add('even');
+                } else {
+                  rows[i].classList.add('odd');
+                }
+              }
+            }
+            
 
-            $searchResult.find('a[href*="//cdn.bitx.tv"]').remove(); // remove weird streaming torrent spam links
+            // remove 'Type' links
+            var types = searchResult.querySelectorAll('td.vertTh a');
+
+            for (i = 0; i < types.length; i++) {
+              var typeParent = types[i].parentNode;
+              while (types[i].firstChild) typeParent.insertBefore(types[i].firstChild, types[i]);
+              typeParent.removeChild(types[i]);
+            }
+
+            // add the Pirate Day domain to User links
+            var users = searchResult.querySelectorAll('.detName a, td a[href*="/user"]');
+
+            for (i = 0; i < users.length; i++) {
+              var oldHref = users[i].getAttribute('href');
+              users[i].setAttribute('href', 'https://thepiratebay.org' + oldHref);
+            }
+
+            // remove weird streaming torrent spam links
+            var bitx = searchResult.querySelectorAll('a[href*="//cdn.bitx.tv"]'); 
+
+            for (i = 0; i < bitx.length; i++) {
+              bitx[i].innerHTML = '';
+              bitx[i].parentNode.removeChild(bitx[i]);
+            }
 
             // apply some IMDB table styles
-            $searchResult.css('border-collapse', 'collapse');
-            $searchResult.find('th').css({'padding-bottom': '10px', 'font-size': '.9em'});
-            $searchResult.find('td').css('padding', '8px 4px 10px');
-            $searchResult.find('tbody tr:nth-child(odd)').addClass('odd');
-            $searchResult.find('tbody tr:nth-child(even)').addClass('even');
+            searchResult.style.borderCollapse = 'collapse';
 
-            $('.impb-loading').remove(); // get that loading text outta there
-            $('#impb').append($searchResult); // get those results in there
+            var th = searchResult.querySelectorAll('th');
+
+            for (i = 0; i < th.length; i++) {
+              th[i].style.paddingBottom = '10px';
+              th[i].style.fontSize = '.9em';
+            }
+
+            var td = searchResult.querySelectorAll('td');
+
+            for (i = 0; i < td.length; i++) {
+              td[i].style.padding = '8px 4px 10px';
+            }
+  
+
+            impbContent.innerHTML = searchResult.outerHTML;
 
           } else { // no stuff :(
-            $('.impb-loading').remove();
-            $('#impb').append('No hits');
+
+            console.log('no stuff');
+            impbContent.innerHTML = 'No hits';
+
           } 
+        } else {
+          // We reached our target server, but it returned an error
+          console.log('Server returned error');
         }
-      });
+      };
+
+      request.onerror = function() {
+        // There was a connection error of some sort
+        console.log('Error connecting to server');
+      };
+
+      request.send();
 
     }
   }, 10);
